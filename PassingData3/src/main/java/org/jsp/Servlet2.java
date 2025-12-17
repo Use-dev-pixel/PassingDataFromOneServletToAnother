aaf2b4404df7a4c5d9e550b694ad5066d40cbbab
Use-dev-pixel <uselessemail9512@gmail.com>
2025-12-17 16:58:27 +0530

    17-12-2025

---
+++ PassingData3/src/main/java/org/jsp/Servlet2.java
@@ -0,0 +1,27 @@
+package org.jsp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet2")
+public class Servlet2 extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email=req.getParameter("email");
+		String password=req.getParameter("password");
+		String name=(String)req.getAttribute("name");
+		
+		System.out.println(email);
+		System.out.println(password);
+		
+		PrintWriter out=resp.getWriter();
+		out.println("<h1>email "+email+" : Passsword "+password+" : Name "+name+"</h1>");
+		
+	}
+}
